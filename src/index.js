d521b4339e051f3636e37ffaa91c1d25406cc8e1
jeremy <jeremy@jeremybeck.io>
2021-09-03 17:25:24 -0700

    commented out debugging

--- src/index.js
+++ src/index.js
@@ -15,21 +15,21 @@ const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
 
 // DEBUGGING - Comment this out
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//     }) : compose;
 
-const enhancer = composeEnhancers(
-  applyMiddleware(thunkMiddleware, logger),
-  // other store enhancers if any
-);
-const store = createStore(rootReducer, enhancer);
+// const enhancer = composeEnhancers(
+//   applyMiddleware(thunkMiddleware, logger),
+//   // other store enhancers if any
+// );
+// const store = createStore(rootReducer, enhancer);
 
 // Release
-// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <React.StrictMode>
